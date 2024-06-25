0db3b487641007c185cedb0652d34d2473a2808d
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2024-06-24 22:21:21 -0300

    :minidisc: developed register and storage applications

--- application/register.c
+++ application/register.c
@@ -5,4 +5,204 @@
  *      Author: pablo-jean
  */
 
+#include "register.h"
 
+/** Privates **/
+
+
+
+/** Publics **/
+register_err_e register_start(register_t *Register){
+	BoardAssert(Register);
+
+	storage_start(&Register->Storage);
+	Register->u32TotalSpace = REGISTER_MAXIMUM_CARDS;
+	register_cards_registed(Register);
+
+	return REGISTER_OK;
+}
+
+uint32_t register_cards_registed(register_t *Register){
+	uint32_t i, address, qtd;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+
+	qtd = 0;
+	address = REGISTER_EE_CARDS_START;
+	for (i=0 ; i<REGISTER_MAXIMUM_CARDS ; i++){
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK){
+			qtd++;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+	Register->u32CardsRegistered = qtd;
+	Register->u32FreeSpace = Register->u32TotalSpace - Register->u32CardsRegistered;
+
+	return qtd;
+}
+
+register_err_e register_check_card(register_t *Register, uint64_t CardID, uint32_t *IDX){
+	uint32_t i, address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+
+	address = REGISTER_EE_CARDS_START;
+	for (i=0 ; i<REGISTER_MAXIMUM_CARDS ; i++){
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK &&
+				RegCard->u64CardID == CardID){
+			if (IDX != NULL){
+				*IDX = i;
+			}
+			return REGISTER_OK;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+
+	return REGISTER_NOT_FOUND;
+}
+
+register_err_e register_add_card(register_t *Register, uint64_t CardID){
+	uint32_t i, freeIDX, address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+
+	if (Register->u32FreeSpace == 0){
+		return REGISTER_NO_FREE_SPACES;
+	}
+
+	// Start with an Invalid Index
+	freeIDX = 0xFF;
+	address = REGISTER_EE_CARDS_START;
+	for (i=0 ; i<REGISTER_MAXIMUM_CARDS ; i++){
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK &&
+				RegCard->u64CardID == CardID){
+			return REGISTER_CARD_ALREADY_EXISTS;
+		}
+		if (RegCard->u8UsedMask == REGISTER_UNUSED_MASK &&
+				freeIDX == 0xFF){
+			freeIDX = i;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+	RegCard->u8UsedMask = REGISTER_USED_MASK;
+	RegCard->u64CardID = CardID;
+	address = REGISTER_EE_CARDS_START + (freeIDX * REGISTER_EE_CARDS_STEP);
+	storage_write(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+
+	Register->u32CardsRegistered++;
+	Register->u32FreeSpace--;
+
+	return REGISTER_OK;
+}
+
+uint32_t register_list_cards(register_t *Register, uint64_t *OutArray){
+	uint32_t i, It, address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+	BoardAssert(OutArray);
+
+	address = REGISTER_EE_CARDS_START;
+	It = 0;
+	for (i=0 ; i<REGISTER_MAXIMUM_CARDS ; i++){
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK){
+			OutArray[It] = RegCard->u64CardID;
+			It++;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+
+	return It;
+}
+
+register_err_e register_del_card_by_ID(register_t *Register, uint64_t CardID){
+	uint32_t i, address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+
+	if (Register->u32CardsRegistered == 0){
+		// we return OK, for easily synchronization
+		return REGISTER_OK;
+	}
+
+	address = REGISTER_EE_CARDS_START;
+	for (i=0 ; i<REGISTER_MAXIMUM_CARDS ; i++){
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK &&
+				RegCard->u64CardID == CardID){
+			RegCard->u64CardID = 0x0;
+			RegCard->u8UsedMask = REGISTER_UNUSED_MASK;
+			storage_write(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+			Register->u32CardsRegistered--;
+			Register->u32FreeSpace++;
+			break;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+	return REGISTER_OK;
+}
+
+register_err_e register_del_card_by_IDX(register_t *Register, uint32_t IDX){
+	uint32_t address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+	BoardAssert(IDX < REGISTER_MAXIMUM_CARDS);
+
+	if (Register->u32CardsRegistered == 0){
+		// we return OK, for easily synchronization
+		return REGISTER_OK;
+	}
+
+	address = REGISTER_EE_CARDS_START + (REGISTER_EE_CARDS_STEP * IDX);
+	storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+	if (RegCard->u8UsedMask == REGISTER_USED_MASK){
+		Register->u32CardsRegistered--;
+		Register->u32FreeSpace++;
+		RegCard->u64CardID = 0x0;
+		RegCard->u8UsedMask = REGISTER_UNUSED_MASK;
+		storage_write(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+	}
+
+	return REGISTER_OK;
+}
+
+register_err_e register_del_all_cards(register_t *Register){
+	uint32_t i, address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+
+	if (Register->u32CardsRegistered == 0){
+		// we return OK, for easily synchronization
+		return REGISTER_OK;
+	}
+
+	RegCard->u64CardID = 0x0;
+	RegCard->u8UsedMask = REGISTER_UNUSED_MASK;
+	address = REGISTER_EE_CARDS_START;
+	for (i=0 ; i<REGISTER_MAXIMUM_CARDS ; i++){
+		storage_write(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+		address += REGISTER_EE_CARDS_STEP;
+	}
+
+	Register->u32CardsRegistered = 0;
+	Register->u32FreeSpace = Register->u32TotalSpace;
+
+	return REGISTER_OK;
+}
--- application/storage.c
+++ application/storage.c
@@ -5,4 +5,192 @@
  *      Author: pablo-jean
  */
 
+#include "storage.h"
 
+/**
+ * Privates
+ */
+
+/** Enumerates **/
+typedef enum{
+	_CMD_WRITE,
+	_CMD_READ
+}_storage_cmd_e;
+
+typedef enum{
+	_RSP_OK,
+	_RSP_FAILED
+}_storage_rsp_e;
+
+/** Structs **/
+typedef struct{
+	_storage_cmd_e eCmd;
+	uint8_t *pu8Buffer;
+	uint32_t u32Len;
+	uint32_t u32Address;
+	QueueHandle_t xRsp;
+}_storage_data_t;
+
+/** Macros **/
+#define EEPROM_SIZE		512
+#define PAGE_SIZE		16
+#define EEPROM_ADDRESS	0xA0
+
+/** Functions **/
+/** EEPROM Functions for Library **/
+eeprom_e EE_Write(uint32_t i2cAddress, uint32_t memAddress, uint8_t memAddrSize, uint8_t *data, uint32_t len){
+if (memAddress >= 256){
+		i2cAddress |= 0x2;
+		memAddress -= 256;
+	}
+	board_i2c_lock();
+	board_eeprom_write(i2cAddress, memAddress, memAddrSize, data, len, 100);
+	board_i2c_unlock();
+
+	return EE_OK;
+}
+
+eeprom_e EE_Read(uint32_t i2cAddress, uint32_t memAddress, uint8_t memAddrSize, uint8_t *data, uint32_t len){
+	if (memAddress >= 256){
+		i2cAddress |= 0x2;
+		memAddress -= 256;
+	}
+	board_i2c_lock();
+	board_eeprom_read( i2cAddress, memAddress, memAddrSize, data, len, 100);
+	board_i2c_unlock();
+
+	return EE_OK;
+}
+
+/** Tasks **/
+
+void _task_storage(void *pvParams){
+	storage_t *storage = (storage_t*)pvParams;
+	_storage_data_t StoData;
+	_storage_rsp_e StoRsp;
+	eeprom_t *Eeprom = &storage->EepromHandle;
+	eeprom_e eeRsp;
+
+	Eeprom->DelayMs = vTaskDelay;
+	Eeprom->EeReadFxn = EE_Read;
+	Eeprom->EeWriteFxn = EE_Write;
+	Eeprom->i2cAddress = EEPROM_ADDRESS;
+	Eeprom->memAddrSize = 1;
+	Eeprom->memSize = EEPROM_SIZE;
+	Eeprom->pageSize = PAGE_SIZE;
+	eeRsp = eeprom_init(Eeprom);
+	BoardAssert(eeRsp == EE_OK);
+
+	storage->xQueueToGatekeeper = xQueueCreate(5, sizeof(_storage_data_t));
+	loop {
+		xQueueReceive(storage->xQueueToGatekeeper, &StoData, portMAX_DELAY);
+
+		switch (StoData.eCmd){
+		case _CMD_WRITE:
+			// execute write
+			eeRsp = eeprom_write(Eeprom,
+					StoData.u32Address,
+					StoData.pu8Buffer,
+					StoData.u32Len);
+			if (StoData.xRsp != NULL){
+				if (eeRsp == EE_OK){
+					StoRsp = _RSP_OK;
+				}
+				else {
+					StoRsp = _RSP_FAILED;
+				}
+				xQueueSend(StoData.xRsp, &StoRsp, 0);
+			}
+			break;
+		case _CMD_READ:
+			/// execute read
+			eeRsp = eeprom_read(Eeprom,
+					StoData.u32Address,
+					StoData.pu8Buffer,
+					StoData.u32Len);
+			if (StoData.xRsp != NULL){
+				if (eeRsp == EE_OK){
+					StoRsp = _RSP_OK;
+				}
+				else {
+					StoRsp = _RSP_FAILED;
+				}
+				xQueueSend(StoData.xRsp, &StoRsp, 0);
+			}
+			break;
+		}
+	}
+}
+
+/* Auxiliary */
+storage_err_e _send_cmd_to_gatekeeper(storage_t *storage, _storage_data_t *StoCmd){
+	_storage_rsp_e StoRsp;
+	QueueHandle_t xWaitQueue;
+
+	while (storage->xQueueToGatekeeper == NULL){
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
+
+	xWaitQueue =xQueueCreate(1, sizeof(_storage_rsp_e));
+
+	StoCmd->xRsp = xWaitQueue;
+	xQueueSend(storage->xQueueToGatekeeper, StoCmd, pdMS_TO_TICKS(200));
+	xQueueReceive(xWaitQueue, &StoRsp, portMAX_DELAY);
+
+	vQueueDelete(xWaitQueue);
+	if (StoRsp == _RSP_FAILED){
+		return STORAGE_FAILED;
+	}
+
+	return STORAGE_OK;
+}
+
+/**
+ * Publics
+ */
+
+void storage_start(storage_t *storage){
+	BaseType_t Err;
+
+	BoardAssert(storage != NULL);
+
+	xErr = xTaskCreate(_task_storage,
+			"Task storage",
+			256,
+			(void*)(storage),
+			BOARD_TASK_PRIO_MID,
+			&storage->xTask);
+	BoardAssert(xErr == pdPASS);
+}
+
+storage_err_e storage_write(storage_t *storage, uint32_t Addr, uint8_t *DatToWrite, uint32_t Len){
+	_storage_data_t StoData;
+
+	BoardAssert(storage != NULL);
+	BoardAssert(DatToWrite != NULL);
+	BoardAssert(Len <= EEPROM_SIZE);
+	BoardAssert((Addr+Len) <= EEPROM_SIZE);
+
+	StoData.eCmd = _CMD_WRITE;
+	StoData.pu8Buffer = DatToWrite;
+	StoData.u32Len = Len;
+	StoData.u32Address = Addr;
+
+	return _send_cmd_to_gatekeeper(storage, &StoData);
+}
+
+storage_err_e storage_read(storage_t *storage, uint32_t Addr, uint8_t *DatToRead, uint32_t Len){
+	_storage_data_t StoData;
+
+	BoardAssert(storage != NULL);
+	BoardAssert(DatToWrite != NULL);
+	BoardAssert(Len <= EEPROM_SIZE);
+	BoardAssert((Addr+Len) <= EEPROM_SIZE);
+
+	StoData.eCmd = _CMD_READ;
+	StoData.pu8Buffer = DatToRead;
+	StoData.u32Len = Len;
+	StoData.u32Address = Addr;
+
+	return _send_cmd_to_gatekeeper(storage, &StoData);
+}
